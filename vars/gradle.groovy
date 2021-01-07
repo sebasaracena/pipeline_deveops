4aacc5a4dddd87b7cd7f402f1c79123115845a91
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-07 18:45:26 -0300

    groovy gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -7,19 +7,6 @@ def call(){
         }
     }
 
-    if(util.validateStage('sonar'))
-    {
-        stage('sonar') {
-            // Nombre extraido desde Jenkins > Global tool configuration > SonarQube Scanner
-            def scannerHome = tool 'sonar-scanner';
-
-            // Nombre extraido desde Jenkins > Configurar el sistema > SonarQube servers
-            withSonarQubeEnv('sonar-server') {
-                bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
-            }
-        }
-    }
-
     if(util.validateStage('run'))
     {
         stage('run') {
@@ -35,31 +22,6 @@ def call(){
         }
     }
 
-    if(util.validateStage('nexus'))
-    {
-        stage('nexus') {
-            nexusPublisher nexusInstanceId: 'NexusLocal',
-                nexusRepositoryId: 'test-nexus',
-                packages: [
-                    [
-                        $class: 'MavenPackage',
-                        mavenAssetList: [
-                            [
-                                classifier: '',
-                                extension: 'jar',
-                                filePath: 'C:\\proyects\\diplomado\\gradle\\ejemplo-gradle\\build\\DevOpsUsach2020-0.0.1.jar'
-                            ]
-                        ],
-                        mavenCoordinate: [
-                            artifactId: 'DevOpsUsach2020',
-                            groupId: 'com.devopsusach2020',
-                            packaging: 'jar',
-                            version: '0.0.1'
-                        ]
-                    ]
-                ]
-        }
-    }
 }
 
 return this;
