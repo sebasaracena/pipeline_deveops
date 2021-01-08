9e777231347b3991dec2321d3db302b92768f7a4
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 09:41:55 -0300

    ultimos cambios jenkins 19

--- vars/mavencd.groovy
+++ vars/mavencd.groovy
@@ -3,7 +3,7 @@ def call(){
    if(util.validateStage('Downloadnexus')){
       stage('Downloadnexus') {
        env.STAGE = 'Downloadnexus'
-       echo env.STAGE
+       figlet env.STAGE
        sh 'curl http://35.199.77.109:8081/repository/realese-v0.1.0/com/devopusach2020/DevOpsUsach2020/1.0.0/DevOpsUsach2020-1.0.0.jar --output DevOpsUsach2020-1.0.0.jar'
       }
    }
@@ -11,7 +11,7 @@ def call(){
     if(util.validateStage('runDownloadjar')){
      stage('runDownloadjar') {
     env.STAGE = 'runDownloadjar'
-    echo env.STAGE
+    figlet env.STAGE
     echo 'Esperando a que termine la descarga'
 	sleep(time: 10, unit: "SECONDS")
      sh 'JENKINS_NODE_COOKIE=dontKillMe nohup start java -jar DevOpsUsach2020-1.0.0.jar &'
@@ -22,7 +22,7 @@ def call(){
     {
         stage('rest') {
             env.STAGE = 'rest'
-            echo env.STAGE
+            figlet env.STAGE
             sleep(time: 10, unit: "SECONDS")
             bat 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }
