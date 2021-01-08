ce9680af49941e7f299724fc6b7c4dbc76e646f9
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 00:04:29 -0300

    nueva carpeta groovy 6

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -3,7 +3,7 @@ def call(){
     if(env.GIT_BRANCH=='origin/release-v0.01'){
         echo 'buena es de la rama release'
     }
-    else if(env.GIT_BRANCH=='origin/feature*' || env.GIT_BRANCH=='origin/develop'){
+    else if(env.GIT_BRANCH=='feature-librery' || env.GIT_BRANCH=='develop'){
        echo 'no es de la rama realease'
 
     } 
