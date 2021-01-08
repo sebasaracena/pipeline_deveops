7054ecbac85edc3ce8b0435c3b9b2c70ddf4078c
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-07 21:29:34 -0300

    pipeline en grove maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -14,12 +14,23 @@ def call(){
         }
     }
 
+   if(util.validateStage('sonar')) {
+   stage('sonar') {
+            steps {
+    withSonarQubeEnv(installationName: 'SonarQube') { 
+      bat 'mvn org.sonarsource.scanner.maven:sonar-maven-plugin:3.7.0.1746:sonar'
+    }
+            }
+    }
+  }
+
     if(util.validateStage('jar'))
     {
         stage('jar'){
             bat './mvnw.cmd clean package -e'
         }
     }
+
      if(util.validateStage('run'))
     {
         stage('run'){
