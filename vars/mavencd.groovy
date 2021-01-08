de28ce4a633156ab442fab4dc7de73d81ceb1f10
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 09:12:01 -0300

    nueva carpeta groovy 13

--- vars/mavencd.groovy
+++ vars/mavencd.groovy
@@ -2,12 +2,14 @@ def call(){
     
    if(util.validateStage('Downloadnexus')){
       stage('Downloadnexus') {
+       fliget env.STAGE
        sh 'curl http://35.199.77.109:8081/repository/realese-v0.1.0/com/devopusach2020/DevOpsUsach2020/1.0.0/DevOpsUsach2020-1.0.0.jar --output DevOpsUsach2020-1.0.0.jar'
       }
    }
 
     if(util.validateStage('runDownloadjar')){
      stage('runDownloadjar') {
+    fliget env.STAGE
     echo 'Esperando a que termine la descarga'
 	sleep(time: 10, unit: "SECONDS")
     sh 'java -jar DevOpsUsach2020-1.0.0.jar'
@@ -17,6 +19,7 @@ def call(){
     if(util.validateStage('rest'))
     {
         stage('rest') {
+           fliget env.STAGE
             bat 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }
     }
