47eaa24066a4086b4ef9bf48dbafa840a3f2a4a5
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 17:56:26 -0300

    cambio 7

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,9 +1,11 @@
 def call(){
-    figlet env.GIT_BRANCH
+    
     if(env.GIT_BRANCH=='origin/release-v0.01'){
+        figlet env.GIT_BRANCH
         mavencd.call()
     }
-    else if(env.GIT_BRANCH=='feature-librery' || env.GIT_BRANCH=='develop' || env.GIT_BRANCH=='	feature-dir-gradle' || env.GIT_BRANCH=='	feature-gradle' ){
+    else if(env.GIT_BRANCH=='feature-librery' || env.GIT_BRANCH=='develop' || env.GIT_BRANCH=='feature-dir-gradle' || env.GIT_BRANCH=='feature-gradle' ){
+      figlet env.GIT_BRANCH
       mavenci.call()
 
     } 
