d1671f3f47a5482479fc26aa7ec40d77386527bc
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-07 23:56:31 -0300

    nueva carpeta groovy 4

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -1,6 +1,6 @@
 def call(){
     figlet env.GIT_BRANCH
-    if(env.GIT_BRANCH=='release-v0.01'){
+    if(env.GIT_BRANCH=='origin/release-v0.01'){
         figlet 'buena es de la rama release'
     }
     else if(env.GIT_BRANCH=='feature*' || env.GIT_BRANCH=='develop'){
