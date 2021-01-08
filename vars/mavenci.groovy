4410c382a437a0a5a2f1f8d81f138de5a9fcee11
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 10:28:42 -0300

    ultimos cambios jenkins 21

--- vars/mavenci.groovy
+++ vars/mavenci.groovy
@@ -3,6 +3,7 @@ def call(){
     if(util.validateStage('build'))
     {
         stage('build') {
+            env.STAGE = 'build'
            figlet env.STAGE
             sh './mvnw.cmd clean compile -e'
         }
@@ -11,6 +12,7 @@ def call(){
     if(util.validateStage('test'))
     {
         stage('test'){
+            env.STAGE = 'test'
             figlet env.STAGE
             bat './mvnw.cmd clean test -e'
         }
@@ -19,6 +21,7 @@ def call(){
    if(util.validateStage('sonar')) 
    {
    stage('sonar') {
+       env.STAGE = 'sonar'
        figlet env.STAGE
         
     withSonarQubeEnv(installationName: 'SonarQube') { 
@@ -31,6 +34,7 @@ def call(){
     if(util.validateStage('jar'))
     {
         stage('jar'){
+            env.STAGE = 'jar'
             figlet env.STAGE
             bat './mvnw.cmd clean package -e'
         }
@@ -39,22 +43,26 @@ def call(){
      if(util.validateStage('run'))
     {
         stage('run'){
+             env.STAGE = 'run'
             figlet env.STAGE
-           sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &'
+           sh 'JENKINS_NODE_COOKIE=dontKillMe nohup start mvnw spring-boot:run &'
         }
     }
 
        if(util.validateStage('rest'))
     {
         stage('rest') {
-            figlet env.STAGE
+            
             env.STAGE = 'rest'
             figlet env.STAGE
             sleep(time: 10, unit: "SECONDS")
             bat 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }
     }
-  
+  stage('nexus'){
+	    env.STAGE = 'nexus'
+	        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: ‘ejemplo-maven-gradle', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: 'C:\Users\usuario}\.jenkins\workspace\gradle-multibranch_develop\build\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+	}
 
 }
 
