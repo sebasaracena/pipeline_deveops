99479d0521c80e8e0169eee0b5ca1059c76c7f24
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 09:28:18 -0300

    ultimos cambios jenkins 15

--- vars/mavencd.groovy
+++ vars/mavencd.groovy
@@ -3,7 +3,7 @@ def call(){
    if(util.validateStage('Downloadnexus')){
       stage('Downloadnexus') {
        env.STAGE = 'Downloadnexus'
-       fliget env.STAGE
+       echo env.STAGE
        sh 'curl http://35.199.77.109:8081/repository/realese-v0.1.0/com/devopusach2020/DevOpsUsach2020/1.0.0/DevOpsUsach2020-1.0.0.jar --output DevOpsUsach2020-1.0.0.jar'
       }
    }
@@ -11,7 +11,7 @@ def call(){
     if(util.validateStage('runDownloadjar')){
      stage('runDownloadjar') {
     env.STAGE = 'runDownloadjar'
-    fliget env.STAGE
+    echo env.STAGE
     echo 'Esperando a que termine la descarga'
 	sleep(time: 10, unit: "SECONDS")
     sh 'java -jar DevOpsUsach2020-1.0.0.jar'
@@ -22,7 +22,7 @@ def call(){
     {
         stage('rest') {
             env.STAGE = 'rest'
-                fliget env.STAGE
+                echo env.STAGE
             bat 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }
     }
