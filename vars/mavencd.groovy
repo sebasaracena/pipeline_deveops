f0d7720ee061cb8c7e2aab6bf0a1ef3ae47d1e25
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 00:44:10 -0300

    nueva carpeta groovy 11

--- vars/mavencd.groovy
+++ vars/mavencd.groovy
@@ -1,14 +1,14 @@
 def call(){
     
    if(util.validateStage('Downloadnexus')){
-       sh 'curl http://35.199.77.109:8081/repository/realese-v0.1.0/com/devopusach2020/DevOpsUsach2020/1.0.0/DevOpsUsach2020-1.0.0.jar --output package/DevOpsUsach2020-1.0.0.jar'
+       sh 'curl http://35.199.77.109:8081/repository/realese-v0.1.0/com/devopusach2020/DevOpsUsach2020/1.0.0/DevOpsUsach2020-1.0.0.jar --output DevOpsUsach2020-1.0.0.jar'
        
    }
 
     if(util.validateStage('runDownloadjar')){
     echo 'Esperando a que termine la descarga'
 	sleep(time: 10, unit: "SECONDS")
-    sh 'java -jar package/DevOpsUsach2020-1.0.0.jar'
+    sh 'java -jar DevOpsUsach2020-1.0.0.jar'
    }
    
     if(util.validateStage('rest'))
