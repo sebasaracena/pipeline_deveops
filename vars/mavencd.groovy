eb97638b720f3371753c49b31f8b9956b8bc6d58
sebassabes <sebastian.andres.aracena@gmail.com>
2021-01-08 23:27:50 -0300

    jenkins cd

--- vars/mavencd.groovy
+++ vars/mavencd.groovy
@@ -27,8 +27,15 @@ def call(){
             bat 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
         }
     }
-  
-  
+    if(util.validateStage('nexuscd'))
+    { 
+  stage('nexuscd'){
+	    env.STAGE = 'nexuscd'
+	    figlet env.STAGE
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'ejemplo-maven-gradle', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: '', filePath: "C:\\\\Users\\\\usuario}\\\\.jenkins\\\\workspace\\\\ipeline-cicd_pipeline-ci_develop\\\\build\\\\DevOpsUsach2020-0.0.1.jar"]], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '1.0.1']]]
+	}
+} 
+
 
 }
 
